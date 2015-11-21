25d29ccdc99cf42d07f34e4e7208be643337bea1
Cole Chamberlin <colechamberlin@gmail.com>
2015-11-20 16:09:38 -0800

    Work on stateProviders and switching states in angular

--- js/app.js
+++ js/app.js
@@ -1,46 +1,46 @@
 //Angular ui-router
-var app = angular.module('app', ['firebase', 'ui.router'])
-//configure pages
-    .config(function($stateProvider) {
+var app = angular.module('app', ['firebase', 'ui.router']);
+    var ref = new Firebase("https://jesse-chamberlin-portfolio.firebaseio.com");
+    var projectsRef = ref.child('projects');
+    //configure pages
+    app.config(function($stateProvider) {
         $stateProvider.state('home', {
             url: '/',
             templateUrl: 'templates/home.html',
-            controller: 'HomeController'
-        })
-        .state('about', {
-            url: '/about',
-            templateUrl: 'templates/about.html',
-            controller: 'AuracleController'
-        })
-        .state('contact', {
-            url: '/contact',
-            templateUrl: 'templates/contact.html',
-            controller: 'AuracleController'
-        })
-        .state('auracle', {
-            url: '/auracle',
-            templateUrl: 'templates/project-view.html',
-            controller: 'AuracleController'
-        })
-        .state('welp', {
-            url: '/welp',
-            templateUrl: 'templates/project-view.html',
-            controller: 'WelpController'
+            controller: 'homeCtrl'
         });
     })
-    .controller('mainCtrl', function($scope, $firebaseArray) {
-        var ref = new Firebase("https://jesse-chamberlin-portfolio.firebaseio.com");
-        var projectsRef = ref.child('projects');
-        $scope.projects = $firebaseArray(projectsRef).$loaded(function(d) {
-            console.log(d);
+    .controller('mainCtrl', function($scope, $firebaseArray, $state) {
+        $scope.projects = $firebaseArray(projectsRef);
+        $scope.projects.$loaded(function(projects) {
+            app.config(function($stateProvider) {
+                projects.forEach(function(element) {
+                    $stateProvider.state(element.$id, {
+                        url: '/projects/' + element.$id,
+                        templateUrl: 'templates/project-view.html',
+                        controller: 'projectViewCtrl'
+                    });
+                });
+            });
         });
+
+
+
+        $scope.redirect = function(url) {
+            console.log($state.current);
+            if($state.current.url === "^") {
+                console.log('up');
+                $state.go(url);
+            } else{
+                $state.go(url);
+            }
+        };
     })
-    .controller('HomeController', function($scope) {
+    .controller('homeCtrl', function($scope, $firebaseArray) {
+        var aboutRef = ref.child('about');
+        $scope.about = $firebaseArray(aboutRef);
 
     })
-    .controller('AuracleController', function($scope) {
+    .controller('projectViewCtrl', function($scope) {
 
-    })
-    .controller('WelpController', function($scope) {
-
-});
+    });
